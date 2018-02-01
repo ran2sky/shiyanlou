91728fbc057538b463b6d53155bfb4dc2081f53f
luochenxi <hechun.hi@gmail.com>
2018-02-01 13:03:17 +0800

    面向过程

---
+++ ex6_1.py
@@ -0,0 +1,9 @@
+main(){
+    dog_name = '旺才'
+    dog_sound = 'wang wang wang...';
+    cat_name = 'kitty';
+    cat_sound = 'miu miu miu...';
+    print(dog_name + ' is making sound ' + dog_sound);
+    print(cat_name + ' is making sound' + cat_sound);
+}
+
